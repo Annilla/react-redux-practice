92184c69d550638d44e4fb9172a72216ba9f1acc
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2019-06-28 16:52:13 +0800

    Git V1

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+function reducer(state, action) {
+  if (action.type === '_changeState') {
+    return action.payload.newState;
+  }
+  return 'state';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const action = {
+  type: '_changeState',
+  payload: {
+    newState: 'New State'
+  }
+};
+
+store.dispatch(action);
+
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
