597a5276a0797285a57bf2cd5c253fd3f75db214
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2019-07-03 15:12:29 +0800

    Git V5

--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -3,15 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { updateUser, apiRequest } from './actions/user-actions'
+import { createSelector } from 'reselect';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.onUpdateUser = this.onUpdateUser.bind(this);
-  }
-
-  componentDidMount () {
-    this.props.onApiRequest();
   }
 
   onUpdateUser(event) {
@@ -46,13 +43,24 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state, props) => {
-  return {
-    products: state.products,
-    user: state.user,
-    userPlusProps: `${state.user} ${props.aRandomProps}`
-  }
-};
+const productsSelector = createSelector(
+  state => state.products,
+  products => products
+);
+
+const userSelector = createSelector(
+  state => state.user,
+  user => user
+);
+
+const mapStateToProps = createSelector(
+  productsSelector,
+  userSelector,
+  (products, user) => ({
+    products,
+    user
+  })
+);
 
 const mapActionsToProps = {
   onUpdateUser: updateUser,
